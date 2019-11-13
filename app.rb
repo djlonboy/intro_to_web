2fbe280d41c3a135b0b312e617defef8dd36e19d
djlonboy <djlonboy@gmail.com>
2019-11-13 17:13:46 +0000

    Completed using forms exercise

--- app.rb
+++ app.rb
@@ -19,7 +19,12 @@ get '/bob' do
   "You're bob"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  @name = params[:name]
   erb(:index)
 end
